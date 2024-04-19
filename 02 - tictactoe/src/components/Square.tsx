7c4a4089ac63b12bc3e4ba1162e97f341ddd3e16
netsamma <netsamma@gmail.com>
2024-04-19 22:15:12 +0200

    Typescript support

--- 02 - tictactoe/src/components/Square.tsx
+++ 02 - tictactoe/src/components/Square.tsx
@@ -1,6 +1,10 @@
 import styled from "styled-components"
 
-const Square = ({symbol}) => {
+interface SquareProps {
+	symbol: string;
+}
+  
+const Square = ({ symbol }: SquareProps) => {
   return (
 	<Div>{symbol}</Div>
   )
@@ -8,10 +12,12 @@ const Square = ({symbol}) => {
 
 export default Square
 
+
 const Div = styled.div`
 	height: 100px;
 	width: 100px;
 	border: 2px solid #BF4F74;
+	color: #BF4F74;
 	display: flex;
 	justify-content: center;
 	align-items: center;
