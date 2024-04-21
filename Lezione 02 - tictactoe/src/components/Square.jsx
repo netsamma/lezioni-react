f0af9ad07ceca97b303f0ff3fd95151146c8aef9
netsamma <netsamma@gmail.com>
2024-04-21 22:51:58 +0200

    Square component

--- Lezione 02 - tictactoe/src/components/Square.jsx
+++ Lezione 02 - tictactoe/src/components/Square.jsx
@@ -3,7 +3,7 @@ import styled from "styled-components"
   
 const Square = (props) => {
   return (
-	<Div>{props}</Div>
+	<Div>{props.value}</Div>
   )
 }
 
@@ -14,9 +14,10 @@ const Div = styled.div`
 	height: 100px;
 	width: 100px;
 	border: 2px solid #BF4F74;
-	color: #BF4F74;
+	color: #ef9db8;
 	display: flex;
 	justify-content: center;
 	align-items: center;
 	font-size: 30px;
 `
+
