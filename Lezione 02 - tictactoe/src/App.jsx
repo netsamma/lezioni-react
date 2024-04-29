c8b9eda14d5ffdbfb2ae3484de3ab51f6614fd00
Ignazio Sammarco <98886195+netsamma@users.noreply.github.com>
2024-04-29 10:42:17 +0200

    Update App at Arces

--- Lezione 02 - tictactoe/src/App.jsx
+++ Lezione 02 - tictactoe/src/App.jsx
@@ -1,17 +1,70 @@
-import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import './App.css'
+import { useState } from 'react'
 import Square from './components/Square'
 
 function App() {
-  const [value, setValue] = useState("X")
+  const [squares, setSquares] =  useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+  
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
+  function handleClick(i) {
+    if (squares[i] || calculateWinner(squares)) {
+      return;
+    }
+    const nextSquares = squares.slice();
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext);
+  }
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
 
   return (
     <>
-      <Square value={value}/>
+      <div className="status">{status}</div>
+      <div className='board'>
+        <Square value = {squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value = {squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value = {squares[2]} onSquareClick={() => handleClick(2)} />
+        <Square value = {squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value = {squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value = {squares[5]} onSquareClick={() => handleClick(5)} />
+        <Square value = {squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value = {squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value = {squares[8]} onSquareClick={() => handleClick(8)} />
+      </div>
     </>
   )
 }
 
 export default App
+
