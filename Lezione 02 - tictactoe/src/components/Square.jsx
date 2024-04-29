f7af2859ecbca74189eacb32125cf53b017d4a6b
Ignazio Sammarco <98886195+netsamma@users.noreply.github.com>
2024-04-29 10:44:19 +0200

    Update Square at Arces

--- Lezione 02 - tictactoe/src/components/Square.jsx
+++ Lezione 02 - tictactoe/src/components/Square.jsx
@@ -1,23 +1,11 @@
-import styled from "styled-components"
+import './Square.css'
 
-  
-const Square = (props) => {
+function Square(props) {
+
   return (
-	<Div>{props.value}</Div>
+    <button className="square" onClick={props.onSquareClick}> {props.value} </button>
   )
+
 }
 
 export default Square
-
-
-const Div = styled.div`
-	height: 100px;
-	width: 100px;
-	border: 2px solid #BF4F74;
-	color: #ef9db8;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	font-size: 30px;
-`
-
